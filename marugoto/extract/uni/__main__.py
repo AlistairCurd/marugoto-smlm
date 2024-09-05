f8d7c45672e9b39aa97bc0db2331ddd917a3263e
Alistair Curd <fbsacu@leeds.ac.uk>
2024-09-05 16:51:36 +0100

    Include UNI feature extractor.

---
+++ marugoto/extract/uni/__main__.py
@@ -0,0 +1,5 @@
+from fire import Fire
+from ._uni import extract_uni_features_
+
+if __name__ == "__main__":
+    Fire(extract_uni_features_)
---
+++ marugoto/extract/uni/_uni.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Extract features with pretrained model from RetCCL: Clustering-guided Contrastive Learning for Whole-slide Image Retrieval
+
+https://github.com/Xiyue-Wang/RetCCL
+
+use like this:
+    python -m marugoto.extract.xiyue_wang \
+        --checkpoint-path ~/Downloads/best_ckpt.pth \
+        --outdir ~/TCGA_features/TCGA-CRC-DX-features/xiyue-wang \
+        /mnt/TCGA_BLOCKS/TCGA-CRC-DX-BLOCKS/*
+"""
+# %%
+import hashlib
+from pathlib import Path
+import timm
+import torch
+import torch.nn as nn
+from marugoto.extract.extract import extract_features_
+
+# %%
+
+
+def extract_uni_features_(
+    *slide_tile_paths: Path, checkpoint_path: str, **kwargs
+):
+    """Extracts features from slide tiles.
+
+    Requirements: 
+        Permission from authors via huggingface:
+            https://huggingface.co/MahmoodLab/UNI
+        Huggingface account with valid login token
+        Downloaded UNI model
+
+    Args:
+        checkpoint_path:  Path to the downloaded model checkpoint file.
+    """
+    # calculate checksum of model
+    sha256 = hashlib.sha256()
+    with open(checkpoint_path, "rb") as f:
+        while True:
+            data = f.read(1 << 16)
+            if not data:
+                break
+            sha256.update(data)
+
+    digest = sha256.hexdigest()
+
+    # From UNI repo instructions, with downloaded model
+    model = timm.create_model(
+        "vit_large_patch16_224",
+        img_size=224, patch_size=16,
+        init_values=1e-5, num_classes=0, dynamic_img_size=True
+        )
+    
+    uni = torch.load(checkpoint_path, map_location='cpu')
+
+    model.load_state_dict(uni, strict=True)
+
+    # And UNI repo instructions say create a transform for normalisation
+    # after this (but we do this later anyway)
+
+    # Try this, as for ctranspath
+    if torch.cuda.is_available():
+        model = model.to('cuda')  # cuda:0 ??
+
+    return extract_features_(
+        slide_tile_paths=slide_tile_paths,
+        model=model.cuda(),
+        model_name=f"mahmood-uni-{digest[:8]}",
+        **kwargs,
+    )
