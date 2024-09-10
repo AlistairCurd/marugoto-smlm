b4cd5d9390d94c6122ae2458b47d5ea153679697
Alistair Curd <fbsacu@leeds.ac.uk>
2024-09-10 11:59:27 +0100

    Change back to patience 8.

--- marugoto/mil/_mil.py
+++ marugoto/mil/_mil.py
@@ -91,7 +91,7 @@ def train(
 
     cbs = [
         SaveModelCallback(fname=f"best_valid"),
-        EarlyStoppingCallback(min_delta=0.00001, patience=30),
+        EarlyStoppingCallback(min_delta=0.00001, patience=8),
         CSVLogger(),
     ]
 
